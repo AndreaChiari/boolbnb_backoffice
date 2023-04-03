9b86149244ab03baa8a4260a63f3c791960ab10b
Alessandro-Villani <alessvill@libero.it>
2023-04-03 12:43:01 +0200

    added apartment sponsorship seeder

--- database/seeders/ApartmentSponsorshipSeeder.php
+++ database/seeders/ApartmentSponsorshipSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Apartment;
 use App\Models\Sponsorship;
+use Carbon\Carbon;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,21 @@ class ApartmentSponsorshipSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $apartments = Apartment::pluck('id')->toArray();
+        $sponsorships = Sponsorship::pluck('id')->toArray();
+        $max_sponsorships = count($sponsorships) - 1;
+
+        foreach ($apartments as $apartment) {
+            if ($faker->boolean()) {
+                $current_apartment = Apartment::find($apartment);
+                $sponsorship = $sponsorships[$faker->numberBetween(0, $max_sponsorships)];
+                $sponsorship_duration = Sponsorship::find($sponsorship)->duration;
+                $start_date = now();
+                $end_date = date_add(now(), date_interval_create_from_date_string("$sponsorship_duration hours"));
+                $current_apartment->sponsorships()->attach($sponsorship, ['start_date' => $start_date, 'end_date' => $end_date]);
+            }
+        }
     }
 }
