fe097f61b322ecac6a8eb66c76507577ed1d5eac
AndreaChiari <chiariandrea94@gmail.com>
2023-04-03 17:15:33 +0200

    add-crud-messages

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
@@ -12,7 +13,9 @@ class MessageController extends Controller
      */
     public function index()
     {
-        //
+        $messages = Message::all();
+
+        return view('admin.apartments.index', compact('apartments'));
     }
 
     /**
