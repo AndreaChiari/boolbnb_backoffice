369e15528e23eaaf4959946d47be620a8050565c
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-03 17:47:31 +0200

    hotfix icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="@yield('icon', asset())" type="image/png">
+    {{-- <link rel="icon" href="@yield('icon', asset())" type="image/png"> --}}
 
     <title>{{ config('app.name', 'BoolBnb') }} | @yield('title') </title>
 
