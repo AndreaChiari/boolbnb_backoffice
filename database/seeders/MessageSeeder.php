fca252a77134b49411a03ae9093d11c7d953ae0f
Alessandro-Villani <alessvill@libero.it>
2023-04-03 10:25:38 +0200

    added message seeder

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\Message;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +13,20 @@ class MessageSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $apartment_ids = Apartment::pluck('id')->toArray();
+        $max = count($apartment_ids) - 1;
+
+        for ($i = 0; $i < 10; $i++) {
+            $new_message = new Message();
+
+            $new_message->apartment_id = $apartment_ids[$faker->numberBetween(0, $max)];
+            $new_message->email = $faker->email();
+            $new_message->object = $faker->sentence();
+            $new_message->content = $faker->paragraph();
+
+            $new_message->save();
+        }
     }
 }
