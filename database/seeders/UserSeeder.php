45175882e8fd9afa59dcad7f9c56204789ab0443
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-03 10:17:51 +0200

    ass userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = config('users');
+
+        foreach ($users as $user) {
+            $new_user = new User();
+
+            $new_user->name = $user['name'];
+            $new_user->email = $user['email'];
+            $new_user->password = $user['password'];
+            $new_user->first_name = $user['first_name'];
+            $new_user->last_name = $user['last_name'];
+            $new_user->birth_date = $user['birth_date'];
+
+            $new_user->save();
+        }
     }
 }
