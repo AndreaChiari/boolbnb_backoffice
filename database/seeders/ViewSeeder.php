a45f85b38f3a2795bf2de1e8d85ffde7b67695c7
Alessandro-Villani <alessvill@libero.it>
2023-04-03 10:50:34 +0200

    added views seeder

--- database/seeders/ViewSeeder.php
+++ database/seeders/ViewSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\View;
+use Carbon\Carbon;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
 
 class ViewSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $apartment_ids = Apartment::pluck('id')->toArray();
+        $max = count($apartment_ids) - 1;
+
+        for ($i = 0; $i < 10; $i++) {
+            $new_view = new View();
+
+            $new_view->apartment_id = $apartment_ids[$faker->numberBetween(0, $max)];
+            $new_view->date = Carbon::now();
+            $new_view->ip = $faker->ipv4();
+
+            $new_view->save();
+        }
     }
 }
