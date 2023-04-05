05c1194fd382e1c4580ea118deaa178c7fba3804
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-05 08:45:35 +0200

    fix layout index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,46 +1,50 @@
 @extends('layouts.app')
 
-@section('title', 'Apartaments')
+@section('title', 'Apartments')
 
 @section('content')
-    <section id="apartaments">
+    <section id="apartments">
         <div class="container py-5 mt-5">
-            <div class="text-center my-4">
+            <div class="d-flex justify-content-end my-4">
                 <a href="{{ route('admin.apartments.create') }}" class="btn btn-success">
-                    <i class="fas fa-plus me-2"></i> Aggiungi un nuovo appartamento
+                    <i class="fas fa-plus"></i>
                 </a>
             </div>
-            <div class="row d-flex g-5 flex-wrap">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
                 @foreach ($apartments as $apartment)
-                    <div class="col-6 my-5" style="height: 800px;">
-                        <a href="{{ route('admin.apartments.show', $apartment['id']) }}"
-                            style="text-decoration: none; color:black">
-                            <div class="card d-flex flex-column-reverse align-items-center h-100 justify-content-between">
-                                <div class="info-index">
-                                    <h5>Informazioni generali</h5>
-                                    <p>Numero stanze: {{ $apartment->rooms }}</p>
-                                    <p>Numero letti: {{ $apartment->beds }}</p>
-                                    <p>Numero bagni: {{ $apartment->bathrooms }}</p>
+                    <div class="col mb-4">
+                        <div class="card h-100 shadow-sm">
+                            <a href="{{ route('admin.apartments.show', $apartment['id']) }}"
+                                style="text-decoration: none; color:black">
+                                <img src="{{ $apartment->getThumbUrl() }}" class="card-img-top"
+                                    alt="{{ $apartment->address }}">
+                            </a>
+                            <div class="card-body d-flex flex-column justify-content-between">
+                                <div>
+                                    <h5 class="card-title">{{ $apartment->address }}</h5>
+                                    <ul class="list-unstyled mb-0">
+                                        <li>Numero stanze: {{ $apartment->rooms }}</li>
+                                        <li>Numero letti: {{ $apartment->beds }}</li>
+                                        <li>Numero bagni: {{ $apartment->bathrooms }}</li>
+                                    </ul>
                                 </div>
-                                <figure class="text-center h-50 w-100">
-                                    <img src="{{ $apartment->getThumbUrl() }}" alt="{{ $apartment->address }}"
-                                        class="img-fluid h-100">
-                                </figure>
-                                <p class="fw-bold fs-4">{{ $apartment->price }} € / notte </p>
-                                <div class="info text-center">
-                                    <h1>apartment {{ $apartment->address }}</h1>
+                                <div>
+                                    <p class="card-text fw-bold fs-4">{{ $apartment->price }} € / notte</p>
+                                    {{-- Bottoni --}}
+                                    <div class="d-flex justify-content-center align-items-center mt-2 mb-2 gap-3">
+                                        <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
+                                            class="btn btn-warning"><i class="fa-solid fa-arrow-up "></i></a>
+                                        <form action="{{ route('admin.apartments.destroy', $apartment->id) }}"
+                                            method="POST" class="deleteForm">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-danger flex-fill" type="submit"><i
+                                                    class="fa-solid fa-trash"></i></button>
+                                        </form>
+                                    </div>
+
                                 </div>
                             </div>
-                        </a>
-                        <div class="d-flex justify-content-center align-items-center mt-2 mb-5 gap-3">
-                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}" class="btn btn-warning"><i
-                                    class="fa-solid fa-arrow-up me-2"></i>Modifica</a>
-                            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST"
-                                class="deleteForm">
-                                @method('DELETE')
-                                @csrf
-                                <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
-                            </form>
                         </div>
                     </div>
                 @endforeach
