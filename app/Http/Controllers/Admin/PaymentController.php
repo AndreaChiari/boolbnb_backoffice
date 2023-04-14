646c99fe08134c9d7849e7d6d40ded4f456f8e0a
AndreaChiari <chiariandrea94@gmail.com>
2023-04-14 12:44:12 +0200

    update-payment

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    public function index()
+    public function index(Apartment $apartment, Sponsorship $sponsorship)
     {
         $gateway = new \Braintree\Gateway([
             'environment' => config('services.braintree.environment'),
@@ -19,10 +19,43 @@ class PaymentController extends Controller
             'publicKey' => config('services.braintree.publicKey'),
             'privateKey' => config('services.braintree.privateKey')
         ]);
-        $apartments = Apartment::where('id', Auth::id())->get();
-        $sponsorships = Sponsorship::where('id', Auth::id())->get();
 
         $token = $gateway->ClientToken()->generate();
         return view('admin.braintree.payment_form', ['token' => $token], compact('apartments', 'sponsorships'));
+    }
+
+    public function checkout(Request $request)
+    {
+        $gateway = new \Braintree\Gateway([
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey')
+        ]);
+        $amount = $request->amount;
+        $nonce =  $request->payment_method_nonce;
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if ($result->success) {
+            $transaction = $result->transaction;
+            // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+            return back()->with('success_messsage', 'Transazione riuscita' . $transaction->id);
+        } else {
+            $errorString = "";
+
+            foreach ($result->errors->deepAll() as $error) {
+                $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+            }
+            return back()->withErrors('Transazione negata' . $result->message);
+            // $_SESSION["errors"] = $errorString;
+            // header("Location: " . $baseUrl . "index.php");
+        }
     }
 }
