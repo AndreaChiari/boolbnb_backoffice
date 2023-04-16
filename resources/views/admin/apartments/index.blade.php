8734d8421a2cc6b3db6a236d7efee0ca83ed1072
AndreaChiari <chiariandrea94@gmail.com>
2023-04-16 13:14:01 +0200

    add-flip card-test

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -21,81 +21,89 @@
                     @endphp
                     <div class="col mb-4">
                         <a href="{{ route('admin.apartments.show', $apartment->id) }}">
-                            <div class="card h-100">
-                                <figure>
-                                    <img src="{{ $apartment->getThumbUrl() }}" class="card-img-top"
-                                        alt="{{ $apartment->address }}">
-                                    <div class="overlay p-3">
-                                        <span class="views d-flex align-items-center"><i
-                                                class="fa-solid fa-eye me-2"></i>{{ count($apartment->views) }}</span>
-                                        @if ($apartment->isSponsored())
-                                            <div class="sponsored d-flex justify-content-evenly px-2">
-                                                <span> SPONSORED </span>
-                                                @include('includes.timer')
-                                            </div>
-                                        @endif
-                                        <form class="visibility" method="POST"
-                                            action="{{ route('admin.apartments.toggle-visibility', $apartment->id) }}">
-                                            @method('PATCH')
-                                            @csrf
-                                            <div class="position-relative mt-3">
-                                                <button class="btn-backoffice position-absolute bottom-50 end-0">
-                                                    @if ($apartment->visibility)
-                                                        <i class="fa-regular fa-eye mx-0"></i>
-                                                    @else
-                                                        <i class="fa-regular fa-eye-slash mx-0"></i>
-                                                    @endif
-                                                </button>
-                                            </div>
-
-                                        </form>
-                                    </div>
-                                </figure>
-                                <div class="card-body">
-                                    <h2 class="card-title text-center">{{ $apartment->name }}</h2>
-                                    <div class="details row text-center justify-content-around mb-3">
-                                        <div class="col">
-                                            <div class="mb-2">
-                                                <i class="fa-solid fa-person-shelter"></i>
-                                            </div>
-                                            <div class="number">{{ $apartment->rooms }}</div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="mb-2">
-                                                <i class="fa-solid fa-bed"></i>
-                                            </div>
-                                            <div class="number">{{ $apartment->beds }}</div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="mb-2">
-                                                <i class="fa-solid fa-restroom"></i>
-                                            </div>
-                                            <div class="number">{{ $apartment->bathrooms }}</div>
-                                        </div>
-                                    </div>
-                                    <div class="buttons d-flex justify-content-center align-items-center">
-                                        <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
-                                            class="btn-backoffice"><i class="fa-regular fa-pen-to-square"></i></a>
-                                        <form action="{{ route('admin.apartments.destroy', $apartment->id) }}"
-                                            method="POST" class="deleteForm">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button class="btn-backoffice" type="submit"><i
-                                                    class="fa-regular fa-trash-can"></i></button>
-                                        </form>
-                                        <a href="{{ route('admin.messages.index', $apartment->id) }}"
-                                            class="btn-backoffice">
-                                            <i class="fa-regular fa-envelope"></i>
-                                            @if ($new_messages_count)
-                                                <div class="messages-notification small text-center">
-                                                    {{ $new_messages_count }}
+                            <div class="card h-100 ">
+                                <div class="flip-card-inner">
+                                    <figure>
+                                        <img src="{{ $apartment->getThumbUrl() }}" class="card-img-top"
+                                            alt="{{ $apartment->address }}">
+                                        <div class="overlay p-3">
+                                            <span class="views d-flex align-items-center"><i
+                                                    class="fa-solid fa-eye me-2"></i>{{ count($apartment->views) }}</span>
+                                            @if ($apartment->isSponsored())
+                                                <div class="sponsored d-flex justify-content-evenly px-2">
+                                                    <span> SPONSORED </span>
+                                                    @include('includes.timer')
                                                 </div>
                                             @endif
-                                        </a>
-                                        <a href="{{ route('admin.sponsorships.index', $apartment->id) }}"
-                                            class="btn-backoffice"><i class="fa-regular fa-circle-up"></i></a>
-                                        <a href="" class="btn-backoffice"><i class="fa-solid fa-chart-line"></i></a>
+                                            <form class="visibility" method="POST"
+                                                action="{{ route('admin.apartments.toggle-visibility', $apartment->id) }}">
+                                                @method('PATCH')
+                                                @csrf
+                                                <div class="position-relative mt-3">
+                                                    <button class="btn-backoffice position-absolute bottom-50 end-0">
+                                                        @if ($apartment->visibility)
+                                                            <i class="fa-regular fa-eye mx-0"></i>
+                                                        @else
+                                                            <i class="fa-regular fa-eye-slash mx-0"></i>
+                                                        @endif
+                                                    </button>
+                                                </div>
+
+                                            </form>
+                                        </div>
+                                    </figure>
+                                    <div class="card-body">
+                                        <h2 class="card-title text-center">{{ $apartment->name }}</h2>
+                                        <div class="details row text-center justify-content-around mb-3">
+                                            <div class="col">
+                                                <div class="mb-2">
+                                                    <i class="fa-solid fa-person-shelter"></i>
+                                                </div>
+                                                <div class="number">{{ $apartment->rooms }}</div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="mb-2">
+                                                    <i class="fa-solid fa-bed"></i>
+                                                </div>
+                                                <div class="number">{{ $apartment->beds }}</div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="mb-2">
+                                                    <i class="fa-solid fa-restroom"></i>
+                                                </div>
+                                                <div class="number">{{ $apartment->bathrooms }}</div>
+                                            </div>
+                                        </div>
+                                        <div class="buttons d-flex justify-content-center align-items-center">
+                                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
+                                                class="btn-backoffice"><i class="fa-regular fa-pen-to-square"></i></a>
+                                            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}"
+                                                method="POST" class="deleteForm">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button class="btn-backoffice" type="submit"><i
+                                                        class="fa-regular fa-trash-can"></i></button>
+                                            </form>
+                                            <a href="{{ route('admin.messages.index', $apartment->id) }}"
+                                                class="btn-backoffice">
+                                                <i class="fa-regular fa-envelope"></i>
+                                                @if ($new_messages_count)
+                                                    <div class="messages-notification small text-center">
+                                                        {{ $new_messages_count }}
+                                                    </div>
+                                                @endif
+                                            </a>
+                                            <a href="{{ route('admin.sponsorships.index', $apartment->id) }}"
+                                                class="btn-backoffice"><i class="fa-regular fa-circle-up"></i></a>
+                                            <a href="" class="btn-backoffice"><i
+                                                    class="fa-solid fa-chart-line"></i></a>
+                                        </div>
                                     </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h1>John Doe</h1>
+                                    <p>Architect & Engineer</p>
+                                    <p>We love that guy</p>
                                 </div>
                             </div>
 
