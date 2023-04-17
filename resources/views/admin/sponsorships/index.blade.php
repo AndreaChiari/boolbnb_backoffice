2d31407bc4e0ed661ff15206b46c585a69b81f8a
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-17 16:26:26 +0200

    fic css sposnor v2

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -4,26 +4,20 @@
 
 @section('content')
     <section class="general">
-        <div class="container p-5 h-50">
-            <div class="row justify-content-center">
-                @foreach ($sponsorships as $sponsorship)
-                    <div class="col-md-4">
-                        <div
-                            class="sponsor-card @if ($sponsorship->duration === 24) sponsor-green @elseif ($sponsorship->duration === 72) sponsor-purple @else sponsor-gold @endif">
-                            <div class="sponsor-header">
-                                <h3>{{ $sponsorship->name }}</h3>
-                            </div>
-                            <div class="sponsor-body">
-                                <h5>Descrizione offerta</h5>
-                                <p>Sponsorizza il tuo appartamento per {{ $sponsorship->duration }} ore</p>
-                                <p>Prezzo: {{ $sponsorship->price }} €</p>
-                            </div>
-                            <div class="sponsor-footer">
-                                <a href="{{ route('admin.payments', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}"
-                                    class="btn btn-primary">Acquista ora</a>
-                            </div>
-                        </div>
+        <div class="container spon">
+            @foreach ($sponsorships as $sponsorship)
+                <div
+                    class="box @if ($sponsorship->duration === 24) green @elseif ($sponsorship->duration === 72) purple @else gold @endif">
+                    <span></span>
+                    <div class="content">
+                        <h2>{{ $sponsorship->name }}</h2>
+                        <p>Sponsorizza il tuo appartamento per {{ $sponsorship->duration }} ore</p>
+                        <p>Prezzo: {{ $sponsorship->price }} €</p>
+                        <a href="{{ route('admin.payments', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}"
+                            class="btn btn-primary">Acquista ora</a>
                     </div>
-                @endforeach
+                </div>
+            @endforeach
+        </div>
     </section>
 @endsection
