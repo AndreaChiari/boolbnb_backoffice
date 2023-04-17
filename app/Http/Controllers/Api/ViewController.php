8b32d4617f434841f3c9b8d56c4e4eb6f7e14760
RobertoSimonini <robertosimonini1@gmail.com>
2023-04-17 13:06:33 +0200

    post api to check ip and views

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\View;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ViewController extends Controller
@@ -11,5 +13,28 @@ class ViewController extends Controller
     {
         $ip = file_get_contents("http://ipecho.net/plain");
         return response()->json($ip);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $now = Carbon::now();
+        $views = View::where('apartment_id', $data['apartment_id'])->get()->toArray();
+        $isUnique = true;
+
+        foreach ($views as $view) {
+            if (($view['ip'] === $data['ip']) && ($now->floatDiffInHours($view['date'], false) > 0)) {
+                $isUnique = false;
+            }
+        }
+        if ($isUnique) {
+            $view = new View();
+
+            $view->apartment_id = $data['apartment_id'];
+            $view->ip = $data['ip'];
+            $view->date = $now;
+
+            $view->save();
+        }
     }
 }
