6c4312ed41bf18be60a290db20ef3ba842f536bb
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-18 15:00:19 +0200

    fix css statistiche

--- resources/views/admin/apartments/statistics/index.blade.php
+++ resources/views/admin/apartments/statistics/index.blade.php
@@ -3,27 +3,33 @@
 @section('title', 'Statistiche')
 
 @section('content')
-    <div class="container py-5 text-white">
-        <h1>Statistiche</h1>
-        <form action="{{ route('admin.statistics.index', $apartment->id) }}" class="mb-5">
-            @csrf
-            <label for="start_date">Data Iniziale</label>
-            <input type="date" id="start_date" name="start_date" value="{{ $start_date }}">
-            <label for="end_date">Data finale</label>
-            <input type="date" id="end_date" name="end_date" value="{{ $end_date }}">
-            <button>Invia</button>
-        </form>
-        <div class="row justify-content-center">
-            @foreach ($views as $view)
-                <div class="col-1 d-flex flex-column justify-content-end align-items-center">
-                    <p class="graph d-flex align-items-end justify-content-center p-2 rounded"
-                        style="height: calc(50px * {{ $view->count }})">
-                        <span>{{ $view->count }}</span>
-                    </p>
-                    <p>{{ $view->date }}</p>
+    <div id="statistics">
+        <div class="container py-5 text-white">
+            <h1>Statistiche</h1>
+            <form action="{{ route('admin.statistics.index', $apartment->id) }}" class="mb-5">
+                @csrf
+                <label for="start_date">Data Iniziale</label>
+                <input type="date" id="start_date" name="start_date" value="{{ $start_date }}">
+                <label for="end_date">Data finale</label>
+                <input type="date" id="end_date" name="end_date" value="{{ $end_date }}">
+                <button class="btz">Invia</button>
+            </form>
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="graph-container">
+                        @foreach ($views as $view)
+                            <div class="graph-bar" style="height: calc(50px * {{ $view->count }})">
+                                <div class="graph-bar-inner"></div>
+                                <div class="graph-bar-overlay">
+                                    <span class="graph-count">{{ $view->count }}</span>
+                                    <span class="graph-date">{{ $view->date }}</span>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
-            @endforeach
+            </div>
         </div>
-    </div>
 
+    </div>
 @endsection
